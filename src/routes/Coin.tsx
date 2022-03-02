795745f2979ec172957934a6d644b6e88d31f5fd
SpearJin <ckdwls2525@gmail.com>
2022-03-02 18:22:27 +0900

    [Feat] 라우터 설정

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Coin() {
+  const { coinId } = useParams();
+  return <>Coin: {coinId}</>;
+}
+
+export default Coin;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Coins() {
+  return <>Coins</>;
+}
+
+export default Coins;
