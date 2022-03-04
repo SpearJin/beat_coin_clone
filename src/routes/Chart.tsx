be2e32fb3410722a93f99c2cd30517320ed85859
SpearJin <ckdwls2525@gmail.com>
2022-03-04 12:20:26 +0900

    [Feat] chart을 위한 데이터 가져오기

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useOutletContext, useParams } from 'react-router-dom';
+import { fetchCoinHistory } from '../api';
+
+interface ChartProps {
+  coinId: string;
+}
 
 function Chart() {
+  const { coinId } = useOutletContext<ChartProps>();
+  const { isLoading, data } = useQuery(['ohlcv', coinId], () => fetchCoinHistory(coinId));
+
   return <h1>Chart</h1>;
 }
 
